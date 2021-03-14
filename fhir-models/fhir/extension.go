d8a713493c7f39afc092ac614af89859bb3679f4
jonnycrunch <jonnycrunch@me.com>
2021-03-14 18:30:54 -0500

    updated stringValue

--- fhir-models/fhir/extension.go
+++ fhir-models/fhir/extension.go
@@ -22,4 +22,5 @@ type Extension struct {
 	Id        *string     `bson:"id,omitempty" json:"id,omitempty"`
 	Extension []Extension `bson:"extension,omitempty" json:"extension,omitempty"`
 	Url       string      `bson:"url" json:"url"`
+	StringValue string 	  `bason:"stringValue" json:"stringValue"`
 }
